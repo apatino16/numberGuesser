7d1c00551644b5574b21a9ae0148569000a769a3
Anny Patino <103137175+apatino16@users.noreply.github.com>
2022-08-09 17:26:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,34 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// Function returns a random integer between 0 and 9
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10)
+};
+
+// Function determines which player (human or computer) wins based on which guess is closest to the target. 
+const compareGuesses = (human, computer, target) => {
+  humanDif = Math.abs(target - human)
+  computerDif = Math.abs(target - computer)
+  if (humanDif <= computerDif) {
+    // human player wins
+    // If tied, human player wins
+    return true
+  } else if (humanDif > computerDif){
+    // Computer player wins
+    return false
+  }
+};
+
+// Function increases the winner’s score after each round
+const updateScore = winner => {
+  if (winner === 'human') {
+  humanScore++
+} else if (winner === 'computer'){
+  computerScore++
+}
+};
+
+//  Function updates the round number after each round
+const advanceRound = () => currentRoundNumber++ ; 
 
